9f9abe387b4b0f4bdcb0f2eab0d7c1ed3e31f128
AhmedSAHassan <155752718+AhmedSAHassan@users.noreply.github.com>
2026-02-04 11:36:40 +0100

    uploading the code for the reads QC and quantification

---
+++ Shell_scripts/isoquant.sh
@@ -0,0 +1,12 @@
+
+isoquant.py --reference reference/GRCh38.primary_assembly.genome.fa.gz \
+  --genedb reference/gencode.v47.annotation.gtf \
+  --bam minimap_align/PAU84062_pass_FAST_barcode01/PAU84062_pass_FAST_barcode01.bam minimap_align/PAU84062_pass_FAST_barcode02/PAU84062_pass_FAST_barcode02.bam minimap_align/PAU84062_pass_FAST_barcode03/PAU84062_pass_FAST_barcode03.bam minimap_align/PAU84062_pass_FAST_barcode04/PAU84062_pass_FAST_barcode04.bam minimap_align/PAU84062_pass_FAST_barcode05/PAU84062_pass_FAST_barcode05.bam minimap_align/PAU84062_pass_FAST_barcode06/PAU84062_pass_FAST_barcode06.bam\
+  --data_type nanopore -o Isoquant/ --complete_genedb\
+  --model_construction_strategy default_ont --report_novel_unspliced true
+
+
+
+
+
+
